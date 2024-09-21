5a84a061ea4a8c315c38a737e3a533042a23f78b
APIVerve Team <hello@apiverve.com>
2024-09-21 01:03:57 -0500

    Bug Fixes

---
+++ BMICalculatorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class BMICalculatorQueryOptions {
+public double weight { get; set; }
+public double height { get; set; }
+public string unit { get; set; }
+}
+
+}
